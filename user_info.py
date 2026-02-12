a49239188b43fa6dc34416819f873150b35eb7cc
Mohit Kharayat <mohitkharayat0@gmail.com>
2026-02-12 19:44:00 +0000

    variable and input program

---
+++ user_info.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+city = input("Enter your city: ")
+
+print("\n--- User Details ---")
+print("Name:", name)
+print("City:", city)
+print("Welcome to pyton fro Devops/Cloud")
